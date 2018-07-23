4a56454cd03813ae21b97982d267ed27aff7386d
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-07-23 16:29:18 +0200

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import '../css/App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      person: {
+        name:'John',
+        age:'25',
+        hometown:'Antwerp',
+        favourites: {
+          color: 'yellow',
+          food: 'curry'
+        }
+      }
+    }
+  }
+  render() {
+    console.log('this.state.person: ',this.state.person)
+
+    // Destructuring for easy acces
+    let { name, age, hometown, favourites: {color, food} } = this.state.person 
+
+    console.log('Easy acces examples',{name},{age})
+
+    return (
+      <div className="App">
+        <p>My name is {this.state.person.name}, I'm from {this.state.person.hometown} and am {this.state.person.age} years old. You can wake me up for {this.state.person.favourites.food} and paint my walls {this.state.person.favourites.color}</p>
+        <p>My name is {name}, I'm from {hometown} and am {age} years old. You can wake me up for {food} and paint my walls {color}</p> 
+      </div>
+    )
+  }
+}
+
+export default App
